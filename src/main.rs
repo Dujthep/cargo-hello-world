8564862daef1f177806939221528bb8c8a957e8b
dujthep-odds <dujthep@odds.team>
2022-04-01 22:43:47 +0700

    try number separator.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let a:f64 = 11_000.55_032;
+    let b:u64 = 2110_23;
+    let c:u64 = 1_0;
+    println!("a value is {}", a);
+    println!("b value is {}", b);
+    println!("c value is {}", c.to_string());
 }
