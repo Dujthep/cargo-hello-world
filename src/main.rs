c3faa8dd46b39b61225d6c5801bcbc0dd6ffa533
dujthep-odds <dujthep@odds.team>
2022-04-01 22:51:12 +0700

    declare variable to mutable.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
     let a:f64 = 11_000.55_032;
     let b:u64 = 2110_23;
-    let c:u64 = 1_0;
+    let mut c:u64 = 1_0;
     println!("a value is {}", a);
     println!("b value is {}", b);
     println!("c value is {}", c.to_string());
+
+    c = 11;
+    println!("c change value to {}", c.to_string());
 }
